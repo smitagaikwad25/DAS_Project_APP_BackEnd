74a09a8b33ea67dc4a95f1c6c713ae8a5b127374
smitagaikwad25 <gaikwadsmita021@gmail.com>
2025-01-14 14:03:26 +0530

    [smita]Added:project initialization

---
+++ src/routes/user.route.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import * as userController from '../controllers/user.controller';
+import { newUserValidator } from '../validators/user.validator';
+import { userAuth } from '../middlewares/auth.middleware';
+
+const router = express.Router();
+
+
+
+export default router;
